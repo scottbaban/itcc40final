f7273eb666f0191b40d657c21b7992f6a1691a8a
Scott Baban <200431995@my.xu.edu.ph>
2023-05-26 23:49:41 +0800

    .

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ const Home = () => {
               BSCS - 3 Student
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              This is a glimpse into my journey, experiences, and passions. Join me as I share my story and showcase my growth as a student. Let's connect and explore the possibilities together!
+            This is a glimpse into my journey, experiences, and passions. Join me as I share my story and showcase my growth as a student. Let's connect and explore the possibilities together!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a
